8e9bcffd4b633a54f141aeb1472a2bb6611d6f50
Tobias <tobias.schwarz1994@live.de>
2018-04-19 10:40:13 +0200

    added method

--- app/src/main/java/edu/example/ssf/mma/data/SectionIdentifier.java
+++ app/src/main/java/edu/example/ssf/mma/data/SectionIdentifier.java
@@ -1,4 +1,11 @@
 package edu.example.ssf.mma.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SectionIdentifier {
+
+    public List<Section> identifySections(List<TickData> data){
+        return new ArrayList<Section>();
+    }
 }
