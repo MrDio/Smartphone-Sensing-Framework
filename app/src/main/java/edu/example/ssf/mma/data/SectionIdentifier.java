53be381affe672f5bdecb51baf1f8f171099b169
Tobias <tobias.schwarz1994@live.de>
2018-04-20 20:45:19 +0200

    Overhauled section distincton completely

--- app/src/main/java/edu/example/ssf/mma/data/SectionIdentifier.java
+++ app/src/main/java/edu/example/ssf/mma/data/SectionIdentifier.java
@@ -1,55 +1,114 @@
 package edu.example.ssf.mma.data;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class SectionIdentifier {
-    private static Section currentSection;
-    private static ArrayList<TickData> temporarySectionPoints = new ArrayList<>();
-    private static boolean curveStarted = false;
-    private static final float CURVETHRESHOLD = 1f;
-    private static final int MINCURVEPOINTS = 3;
-    private static final int NUMBEROFPOINTSCONSIDERED = 4;
-    private static boolean firstSection= false;
+    private static HashMap<Integer,TickData> pointsUnderThreshold = new HashMap<>();
+    private static HashMap<Integer,TickData> pointsOverThreshold = new HashMap<>();
+    private static TreeMap<Integer,Section> SectionList = new TreeMap<>();
 
+    private static final float CURVETHRESHOLD = 1f;
 
     public static ArrayList<Section> identifySections(ArrayList<TickData> data){
-        curveStarted = false;
-        firstSection = true;
         ArrayList<Section> sections = new ArrayList<>();
-        currentSection = new Section();
-        currentSection.setStart(data.get(0));
-        currentSection.setMedian(new TickData());
-        currentSection.setType(Section.SectionType.UNDEFINED);
-        for (TickData tickData : data) {
-            if(Math.abs(tickData.getAccX()) >= CURVETHRESHOLD && !curveStarted && temporarySectionPoints.size() >= 3){
-                curveStarted = true;
-                currentSection.setEnd(tickData);
-                currentSection.setTimeTaken();
-                TickData median = calculateMedian(temporarySectionPoints);
-                currentSection.setMedian(median);
-                sections.add(currentSection);
-                currentSection = new Section();
-                currentSection.setStart(tickData);
-            } else if(curveStarted && Math.abs(tickData.getAccX()) <= CURVETHRESHOLD && temporarySectionPoints.size() >= 3){
-                curveStarted = false;
-                currentSection.setEnd(tickData);
-                currentSection.setTimeTaken();
-                TickData median = calculateMedian(temporarySectionPoints);
-                currentSection.setMedian(median);
-                sections.add(currentSection);
-                currentSection = new Section();
-                currentSection.setStart(tickData);
-                currentSection.setType(Section.SectionType.STRAIGHT);
-                temporarySectionPoints = new ArrayList<>();
+
+        for (int i = 0; i < data.size(); i++) {
+            if(Math.abs(data.get(i).getAccX())>CURVETHRESHOLD)
+                pointsOverThreshold.put(i,data.get(i));
+            else{
+                pointsUnderThreshold.put(i,data.get(i));
             }
-            temporarySectionPoints.add(tickData);
+
         }
-        currentSection.setEnd(data.get(data.size()-1));
-        currentSection.setTimeTaken();
-        TickData median = calculateMedian(temporarySectionPoints);
-        currentSection.setMedian(median);
-        sections.add(currentSection);
+        putLonelyPointsFromUpperBorderIntoLower();
+        SectionList = createSectionsInLowerDataset(SectionList,pointsUnderThreshold);
+        SectionList = createSectionsInUpperDataset(SectionList,pointsOverThreshold);
+
+        for (Map.Entry<Integer,Section> entry: SectionList.entrySet()) {
+            sections.add(entry.getValue());
+        }
+
         return sections;
+    }
+    private static TreeMap<Integer,Section> createSectionsInLowerDataset(TreeMap<Integer,Section> sectionMap, HashMap<Integer,TickData> lowerBoundDataset){
+        int i = Collections.min(lowerBoundDataset.keySet());
+        int max = Collections.max(lowerBoundDataset.keySet());
+
+        while (i < max) {
+            Section section = new Section();
+            ArrayList<TickData> temporaryList = new ArrayList<>();
+            temporaryList.add(lowerBoundDataset.get(i));
+            int key = i;
+            while(lowerBoundDataset.containsKey(i)){
+                temporaryList.add(lowerBoundDataset.get(i));
+                i++;
+            }
+
+            section.setStart(temporaryList.get(0));
+            section.setEnd(temporaryList.get(temporaryList.size()-1));
+            section.setMedian(calculateMedian(temporaryList));
+            sectionMap.put(key,section);
+            while(!lowerBoundDataset.containsKey(i) && i<max){
+                i++;
+            }
+        }
+        return sectionMap;
+    }
+
+    private static TreeMap<Integer,Section> createSectionsInUpperDataset(TreeMap<Integer,Section> sectionMap, HashMap<Integer,TickData> upperBoundDataset){
+        int i = Collections.min(upperBoundDataset.keySet());
+        int max = Collections.max(upperBoundDataset.keySet());
+        while (i < max) {
+            Section section = new Section();
+            ArrayList<TickData> temporaryList = new ArrayList<>();
+            temporaryList.add(pointsUnderThreshold.get(i-1));
+            int key = i-1;
+            while(upperBoundDataset.containsKey(i)){
+                temporaryList.add(upperBoundDataset.get(i));
+                i++;
+            }
+
+            section.setStart(temporaryList.get(0));
+            section.setEnd(pointsUnderThreshold.get(i));
+            section.setMedian(calculateMedian(temporaryList));
+            sectionMap.put(key,section);
+            while(!upperBoundDataset.containsKey(i) && i<max){
+                i++;
+            }
+        }
+        return sectionMap;
+    }
+
+    private static void putLonelyPointsFromUpperBorderIntoLower(){
+        List<Integer> toRemove = new ArrayList<>();
+        for (Map.Entry<Integer,TickData> entry: pointsOverThreshold.entrySet()) {
+            if(CheckLonelinessOfPoint(entry)){
+                pointsUnderThreshold.put(entry.getKey(),entry.getValue());
+                toRemove.add(entry.getKey());
+            }
+        }
+        for (Integer key: toRemove) {
+            pointsOverThreshold.remove(key);
+        }
+    }
+
+    private static boolean CheckLonelinessOfPoint(Map.Entry<Integer,TickData> entry){
+        if(entry.getKey() > 0 && entry.getKey()<pointsOverThreshold.size()-1){
+            if(!pointsOverThreshold.containsKey(entry.getKey()-1) && !pointsOverThreshold.containsKey(entry.getKey()+1))
+                return true;
+        } else if(entry.getKey()==0 && !pointsOverThreshold.containsKey(entry.getKey()+1))
+        {
+            return true;
+        }else if(entry.getKey()==pointsOverThreshold.size()-1 && !pointsOverThreshold.containsKey(entry.getKey()-1))
+        {
+            return true;
+        }
+        return false;
     }
 
     private static TickData calculateMedian(ArrayList<TickData> curveData){
@@ -66,26 +125,5 @@ public class SectionIdentifier {
         result.setAccY(addedY/curveData.size());
 
         return result;
-    }
-
-    private static boolean CheckNextPointsGREATERThanThreshold(ArrayList<TickData> dataSet, TickData origin, float threshold, int checkRange){
-        int originIndex = dataSet.indexOf(origin);
-        if(originIndex+checkRange >= dataSet.size())
-            return false;
-        for (int i = 0; i < checkRange; i++) {
-            if(Math.abs(dataSet.get(originIndex+i).getAccX())<threshold)
-             return false;
-        }
-        return true;
-    }
-    private static boolean CheckNextPointsSMALLERThanThreshold(ArrayList<TickData> dataSet, TickData origin, float threshold, int checkRange){
-        int originIndex = dataSet.indexOf(origin);
-        if(originIndex+checkRange >= dataSet.size())
-            return false;
-        for (int i = 0; i < checkRange; i++) {
-            if(Math.abs(dataSet.get(originIndex+i).getAccX())>threshold)
-             return false;
-        }
-        return true;
     }
 }
