83cd280ab8f9f6133e8b889aaf61627c94d7ae70
Dennis Mueller <dennis.briefkasten@gmail.com>
2018-04-25 20:13:05 +0200

    added architecture chapter

--- notes/auto/notes.el
+++ notes/auto/notes.el
@@ -2,44 +2,47 @@
  "notes"
  (lambda ()
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "/Users/dennismuller/dotfiles/networkAssignmentConfig")
    (LaTeX-add-labels
-    "sec:org2c9ed28"
-    "sec:orgd208c4b"
-    "sec:org210bdc0"
-    "sec:org86b9b52"
-    "sec:orge6ac6ca"
-    "sec:org493528f"
-    "sec:org463bcd9"
-    "sec:org01eb3ff"
-    "sec:orga08a9a1"
-    "sec:org9e5e015"
-    "sec:org56b6177"
-    "sec:orgee0256e"
-    "sec:orgf213c4e"
-    "sec:org8b90d67"
-    "sec:orgda4522d"
-    "sec:org624e913"
-    "sec:orga3fb9a6"
-    "sec:orgd655d2f"
-    "sec:org0929265"
-    "sec:org6306cc8"
-    "sec:orgeae9f73"
-    "sec:orgb2b1f54"
-    "sec:orgdd19366"
-    "sec:orgbb94b3e"
-    "sec:orge379dce"
-    "sec:orga2e3370"
-    "sec:org7f5b138"))
+    "sec:orgd639853"
+    "sec:org7d7a9e6"
+    "sec:org0041e00"
+    "sec:orgf0412a9"
+    "sec:org93ace6f"
+    "sec:org11f3563"
+    "sec:orgc78ecb4"
+    "sec:org29c2ba8"
+    "sec:orgfdfd3aa"
+    "sec:orgcc3314e"
+    "sec:org7c927b3"
+    "sec:orgaf5a63b"
+    "sec:orgd3a5b2d"
+    "sec:org393d659"
+    "sec:org9ea54c6"
+    "sec:org0a1c43f"
+    "sec:orgd2cdde2"
+    "sec:org2938786"
+    "sec:org3d3dca0"
+    "sec:orgdeb052b"
+    "sec:orgc19d2ea"
+    "sec:org9a10a6c"
+    "sec:org4afd87a"
+    "sec:org350e527"
+    "sec:orgfa9fd4b"
+    "sec:orgb5557bc"
+    "sec:org5420eeb"
+    "sec:org962ce9e"
+    "sec:org5263181"
+    "sec:orge187f87"))
  :latex)
 
