17f15000156b7bb4b274bddcaf9f7214acc78118
Stephan Dittmann <sd085@hdm-stuttgart.de>
2018-04-26 15:30:33 +0200

    Final project commit

--- app/src/main/java/edu/example/ssf/mma/userInterface/MainActivity.java
+++ app/src/main/java/edu/example/ssf/mma/userInterface/MainActivity.java
@@ -26,20 +26,14 @@ package edu.example.ssf.mma.userInterface;
  * @author D. Lagamtzis
  * @version 2.0
  */
-import android.Manifest;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
@@ -62,9 +56,6 @@ public class MainActivity extends AppCompatActivity {
     // Init HW-Factory
     HardwareFactory hw;
 
-    //Permissions Android
-    public  static final int PERMISSIONS_MULTIPLE_REQUEST = 123;
-
     // UI
     private TextView instructionText;
     private ImageView light1;
@@ -86,8 +77,10 @@ public class MainActivity extends AppCompatActivity {
         //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        hw = new HardwareFactory(this);
+        stateMachineHandler = new StateMachineHandler(this);
 
-        ActivityCompat.requestPermissions(MainActivity.this, new  String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_MULTIPLE_REQUEST);
+
 
         //UI -- Textviews init
         instructionText = findViewById(R.id.instructionText);
@@ -117,19 +110,6 @@ public class MainActivity extends AppCompatActivity {
                 onClickMount(view);
             }
         });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[]grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_MULTIPLE_REQUEST:
-                //
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Hardware
-                    hw = new HardwareFactory(this);
-                    stateMachineHandler = new StateMachineHandler(this);
-                }
-        }
     }
 
     public void onClickMount(View v){
--- app/src/main/java/edu/example/ssf/mma/userInterface/StartActivity.java
+++ app/src/main/java/edu/example/ssf/mma/userInterface/StartActivity.java
@@ -1,27 +1,26 @@
 package edu.example.ssf.mma.userInterface;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import edu.example.ssf.mma.R;
-import edu.example.ssf.mma.config.ConfigApp;
+
 
 public class StartActivity extends AppCompatActivity {
-
+    //Permissions Android
+    public  static final int PERMISSIONS_MULTIPLE_REQUEST = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Removes title bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);    // Removes notification bar
-
+        ActivityCompat.requestPermissions(StartActivity.this, new  String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_MULTIPLE_REQUEST);
         setContentView(R.layout.activity_start);
     }
 
