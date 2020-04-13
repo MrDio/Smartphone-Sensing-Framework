c66dd43abcec44a90cc4ee83911d9ffe46f40c15
Kevin Linsmeier <kevin.linsmeier@it-designers.de>
2020-04-13 13:01:33 +0200

    Added new class ClassifierTask to run the classifier asynchronously.

---
+++ app/src/main/java/edu/croptomato/classifier/ClassifierTask.java
@@ -0,0 +1,65 @@
+package edu.croptomato.classifier;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+import edu.croptomato.classifier.Classifier.Recognition;
+import edu.croptomato.ui.MainActivity;
+import edu.croptomato.helper.ImageConverter;
+
+/**
+ * Classifier task to be executed asynchronously.
+ */
+public class ClassifierTask extends AsyncTask<Image, Void, List<Recognition>> {
+
+    @SuppressLint("StaticFieldLeak")
+    private MainActivity activity;
+
+    public ClassifierTask(final MainActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected List<Recognition> doInBackground(final Image... images) {
+        // Convert image to bitmap because the provided classifier requires bitmap input.
+        Bitmap convertedBitmap = ImageConverter.convertImageToBitmap(images[0]);
+        return activity.getClassifier().recognizeImage(convertedBitmap);
+    }
+
+    @Override
+    protected void onPostExecute(final List<Recognition> recognitions) {
+        super.onPostExecute(recognitions);
+
+        float percentageUnripe = 0.0f;
+        float percentageSemiRipe = 0.0f;
+        float percentageRipe = 0.0f;
+
+        // Process all recognitions and their confidence values.
+        for (Recognition recognition : recognitions) {
+            switch (recognition.getTitle()) {
+                case "unripe":
+                    percentageUnripe = recognition.getConfidence();
+                    break;
+                case "halfripe":
+                    percentageSemiRipe = recognition.getConfidence();
+                    break;
+                case "ripe":
+                    percentageRipe = recognition.getConfidence();
+                    break;
+            }
+        }
+
+        // Update the GUI main activity.
+        activity.getNextUpdate().set(System.nanoTime());
+        activity.updateGuiElements(percentageUnripe, percentageSemiRipe, percentageRipe);
+    }
+}
